a4405b1ab5617f0fe018514038ee0a3ef2e1d01a
Muhammad Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-05-09 13:34:14 +0500

    Create differential_eq.py

---
+++ Python/differential_eq.py
@@ -0,0 +1,13 @@
+## Differential Equations.
+from sympy.interactive import printing
+printing.init_printing(use_latex=True)
+from sympy import *
+import sympy as sp
+
+x = sp.Symbol('x')
+y = sp.Function('y')(x)
+
+diffeq = Eq(y.diff(x,x),x**(1/2))
+display(diffeq)
+
+dsolve(diffeq,y)
