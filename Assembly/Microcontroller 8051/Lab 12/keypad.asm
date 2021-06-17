fb00a8504fa7a6f6328e2f4c39fa0fc199d4d849
Muhammad Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-06-18 02:18:56 +0500

    Update keypad.asm

--- Assembly/Microcontroller 8051/Lab 12/keypad.asm
+++ Assembly/Microcontroller 8051/Lab 12/keypad.asm
@@ -27,40 +27,58 @@ jnb p2.1,l5		                    ;jump to l5 if p2.1 is not set and when set got
 jnb p2.0,l6	                      ;jump to l6 if p2.1 is not set and when set goto next instruction.
 jmp next	                        ;continue process.
 row3:
-mov p2,#0ffh		;making port 2 input port or logic 1 mov p2,#00000111b	;making port p2.0,p2.1,p2.2 logic 0
-jnb p2.2,l7	;jump to l7 if p2.2 is not and when set goto next instruction
-jnb p2.1,l8		;jump to l8 if p2.1 is not and when set goto next instruction	jnb p2.0,l9	;jump to l9 if p2.1 is not and when set goto next instruction
-jmp next	;continue process row4:
-mov p2,#0ffh	;making port 2 input port or logic 1 mov p2,#00000111b
-;making port p2.0,p2.1,p2.2 logic 0    jnb p2.2,star		;jump to star if p2.2 is not and when set goto next instruction    jnb p2.1,zero	;jump to zero if p2.1 is not and when set goto next instruction    jnb p2.0,hash	;jump to hash if p2.1 is not and when set goto next instruction
-jmp next	;continue process l1:
-mov p1,#11111001b	;giving 1 to port p1
+mov p2,#0ffh		                  ;making port 2 input port or logic 1.
+mov p2,#00000111b	                ;making port p2.0,p2.1,p2.2 logic 0.
+jnb p2.2,l7	                      ;jump to l7 if p2.2 is not and when set goto next instruction.
+jnb p2.1,l8                       ;jump to l8 if p2.1 is not and when set goto next instruction.
+jnb p2.0,l9	                      ;jump to l9 if p2.1 is not and when set goto next instruction.
+jmp next	                        ;continue process row4:
+mov p2,#0ffh	                    ;making port 2 input port or logic 1.
+mov p2,#00000111b                 ;making port p2.0,p2.1,p2.2 logic 0.
+jnb p2.2,star		                  ;jump to star if p2.2 is not and when set goto next instruction.
+jnb p2.1,zero	                    ;jump to zero if p2.1 is not and when set goto next instruction.
+jnb p2.0,hash	                    ;jump to hash if p2.1 is not and when set goto next instruction.
+jmp next	                        ;continue process l1:
+mov p1,#11111001b	                ;giving 1 to port p1
 jmp next
 
 l2:
-mov p1,#10100100b	;giving 2 to port p1 jmp next
+mov p1,#10100100b	                ;giving 2 to port p1
+jmp next
 
 l3:
-mov p1,#10110000b	;giving 3 to port p1 jmp next
+mov p1,#10110000b	                ;giving 3 to port p1
+jmp next
 
 l4:
-mov p1,#10011001b	;giving 4 to port p1 jmp next
+mov p1,#10011001b	                ;giving 4 to port p1
+jmp next
 
 l5:
-mov p1,#10010010b	;giving 5 to port p1 jmp next
+mov p1,#10010010b	                ;giving 5 to port p1
+jmp next
 
 l6:
-mov p1,#10000010b	;giving 6 to port p1
+mov p1,#10000010b	                ;giving 6 to port p1
 jmp next
 
 l7:
-mov p1,#11111000b jmp next
-l8:
-mov p1,#10000000b jmp next
-l9:
-mov p1,#10010000b jmp next
-star:	mov p1,#10111111bport p1	jmp next
+mov p1,#11111000b 
+jmp next
 
-zero:	mov p1,#11000000b	;giving 0 to port p1		jmp next
-hash: port p1mov p1,#10111111b jmp next
+l8:
+mov p1,#10000000b
+jmp next
+
+l9:
+mov p1,#10010000b
+jmp next
+
+star:	mov p1,#10111111bport p1
+jmp next
+
+zero:	mov p1,#11000000b	;giving 0 to port p1
+jmp next
+hash: port p1mov p1,#10111111b
+jmp next
 end
