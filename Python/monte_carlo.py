4643ab71ada0af906fe5c27cc2ed49e3e80c9114
Muhammad Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-07-02 22:50:18 +0500

    Update monte_carlo.py

--- Python/monte_carlo.py
+++ Python/monte_carlo.py
@@ -1,18 +1,15 @@
-##Monte Carlo Integration
-
 from scipy import random
 import numpy as np
 import matplotlib.pyplot as plt
 
 a = 0
-b = np.pi  ## Limits of the integral.
-N = 10000
+b = np.pi
+N = 100000
 xrand = random.uniform(a,b,N)
 
-def func(x):
-    return np.sin(x)
-    integral = 0.0
-    for i in range(N):
-        integral += func(xrand[i])
-        answer = (b-a)/float(N)*integral
-        print ("The integral of sin(x) = ", answer)
+integral = 0.0
+for i in range(N):
+    integral += np.sin(xrand[i])
+
+ans = ((b-a)/float(N))*integral
+print("integration of sine(x) over a period is: ", ans)
