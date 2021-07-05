c8148a2a435a0b2d5d9ad4eefc0675a116cf11e7
Muhammad Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-07-05 12:37:48 +0500

    Update air_gauge.py

--- Python/air_gauge.py
+++ Python/air_gauge.py
@@ -4,7 +4,7 @@ from numpy import *
 R = 287.058                                   #Specific Gas Constant for Dry Air. [J/Kg.K]
 d0 = 1.2922                                   #Density of Air at 273.17 K.
 M = 0.02897                                   #Molar Mass of Dry Air. [Kg/mol]
-b = 3.4E-3                                    #Volumteric Expansion Coefficient of Air. [1/K]
+b = 3.4E-3                                    #Volumteric Expansion Coefficient of Air. [1/deg. C]
 
 T_values = []
 P_values = []
@@ -12,12 +12,16 @@ P_values = []
 start = 1
 while start == 1:
     T = float(input("Temperature [K]: "))
-    T_values.append(T)
-    d = d0 * exp(b * T)
-    P = (d * R * T)/M
-    P_values.append(P)
-    print("Pressure [Pa]: ", format(P, '.3e'))
-    start = int(input("Press 1 to Start: "))
+    if T > 0:
+        T_values.append(T)
+        d = d0 * exp(b * (T - 273.16) )
+        P = (d * R * T)/M
+        P_values.append(P)
+        print("Pressure [Pa]: ", format(P, '.3e'))
+        start = int(input("Press 1 to Start: "))
+    else:
+        print("Absolute Zero.")
+        start = int(input("Press 1 to Start: "))
 
 plot(T_values,P_values)
 xlabel("Temperature. [K]")
