54dea49fe068c488777797d982f14c6372b77ee9
Muhammad Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-07-09 16:19:42 +0500

    Update lap_timer.py

--- Python/lap_timer.py
+++ Python/lap_timer.py
@@ -4,16 +4,16 @@
 # Programming Exercise.
 # Q 3. Lap Times.
 
-track_round = int(input("Enter the number of rounds: "))   # number of times run around racetrack.
+round_count = int(input("Enter the number of rounds: "))   # number of times run around racetrack.
 time = 0                                                   # timer at initial state.
 time_array = []
 
-for timer in range(track_round):
+for timer in range(round_count):
     lap_time = float(input("Enter Lap Time: t = "))        
     time_array.append(lap_time)
     time += lap_time
 
-average_time = time/track_round
+average_time = time/round_count
 fastest_time = max(time_array)
 slowest_time = min(time_array)
 print("Fastest Lap Time: ", fastest_time)
