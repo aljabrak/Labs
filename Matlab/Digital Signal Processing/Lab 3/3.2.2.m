a14b97a406465b01545bd6d7e33b53e7627fc2d8
Saud Zahir <76210541+aljabrak@users.noreply.github.com>
2021-10-05 22:51:48 +0500

    Update 3.2.2.m

--- Matlab/Digital Signal Processing/Lab 3/3.2.2.m
+++ Matlab/Digital Signal Processing/Lab 3/3.2.2.m
@@ -1,15 +1,4 @@
-clear
 clc
-load lighthouse
-imshow(xx);
-ww200 = xx(200,:);
-disp(ww200);
-
-stem(ww200);
-p = 3;
-xx3 = xx(1:p:end, 1:p:end);
-imshow(xx3);
-
 
 xr1 = (-2).^(0:6);
 L = length(xr1);
