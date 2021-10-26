a61fb0ab62dc0e530fd43170b811f33426c15840
Saud Zahir <76210541+aljabrak@users.noreply.github.com>
2021-10-26 17:03:25 +0500

    Update FFT.py

--- Mathematics/FFT.py
+++ Mathematics/FFT.py
@@ -3,8 +3,8 @@ import os
 
 W_n = []
 N = int(input('Enter Period: N = '))
-for i in range(0, N+1):
-    z = complex(cos(2*pi/N), sin(2*pi/N))
+for i in range(1, N+1):
+    z = complex(cos(2*pi/i), sin(2*pi/i))
     W_n.append(z)
 
 #print(W_n)
@@ -12,6 +12,7 @@ for i in range(0, N+1):
 with open("fft.txt", 'w') as file:
     for a in W_n:
         file.writelines(str(a))
+        file.writelines("\n")
 
 with open("fft.txt", 'r') as file:
     text = file.readlines()
