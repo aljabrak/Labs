3e319741fa146daf48e0b8201b141a8064bef3d8
Saud Zahir <76210541+aljabrak@users.noreply.github.com>
2021-11-24 14:25:50 +0500

    Update phase_modulation.m

--- Matlab/Communication System/Lab 7/phase_modulation.m
+++ Matlab/Communication System/Lab 7/phase_modulation.m
@@ -1,29 +1,29 @@
 % Frequency Modulation.
 
-t = 0:0.001:1;
-fc = 10;
-fm = 2;
+t = 0: 0.001: 1;
+fc = 500;
+fm = 35;
 Am = 1;
 Ac = 1;
-Ct = Ac*cos(2*pi*fc*t);
-Mt = Am*cos(2*pi*fm*t);
-kp = 2;
-St = Ac*cos(2*pi*fc*t+kp*Mt);
+c = Ac*cos(2*pi*fc*t);
+m = Am*cos(2*pi*fm*t);
+kf = 70;
+s = fmmod(m, fm, 3*fm, kf);
 
 subplot(3,1,1)
-plot(t, Mt)
+plot(t, m)
 ylabel('Amplitude');
-xlabel('Time Index');
+xlabel('Time');
 title('Message Signal');
 
 subplot(3,1,2)
-plot(t, Ct)
+plot(t, c)
 ylabel('Amplitude');
-xlabel('Time Index');
+xlabel('Time');
 title('Carrier Signal');
 
 subplot(3,1,3)
-plot(t, St)
+plot(t, s)
 ylabel('Amplitude');
-xlabel('Time Index');
+xlabel('Time');
 title('Phase Modulated Signal');
