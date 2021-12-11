3667e623801b98a6315e7a950aa0442c99117911
Saud Zahir <76210541+aljabrak@users.noreply.github.com>
2021-12-11 23:17:43 +0500

    Machine Learning and Digital Processing

---
+++ Projects/Digital Signal Processing/test.py
@@ -0,0 +1,11 @@
+from numpy import *
+from matplotlib.pyplot import *
+from scipy import *
+import pandas
+from sklearn import *
+
+file = 'earthquakes.csv'
+names = ['impact.gap', 'impact.magnitude', 'location.depth', 'location.distance']
+
+data = pandas.read_csv(file, names)
+print(data)
