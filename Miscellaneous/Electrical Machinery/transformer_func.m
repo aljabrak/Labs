ab510f2f76ef35738e23b546a4108745f93eda69
Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-12-15 17:17:27 +0500

    Add files via upload

---
+++ Miscellaneous/Electrical Machinery/transformer_func.m
@@ -0,0 +1,5 @@
+function [V2, I2] = transformer_func(V1, I1, a)
+V2 = V1/a;
+I2 = a*I1;
+end
+
---
+++ Miscellaneous/Mathematics/collatz_conjecture.py
@@ -0,0 +1,27 @@
+"""
+An orbit is what you get if you start with
+a number and apply a function repeatedly, taking
+each output and feeding it back into the function
+as a new input. There are infinitely many numbers
+whose Collatz orbits pass through 1.
+https://www.quantamagazine.org/why-mathematicians-still-cant-solve-the-collatz-conjecture-20200922/
+"""
+
+
+def f(n):
+    if n % 2 == 0:
+        a = n / 2
+        print(a)
+        f(a)
+
+    elif n == 1:
+        n = n
+        
+    else:
+        a = 3 * n + 1
+        print(a)
+        f(a)
+
+        
+N = 10**20
+f(N)
