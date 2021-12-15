02911dc9ec6fed42c5a6f576fcd2af30f285e6f8
Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-12-15 16:35:30 +0500

    Delete Python directory

--- Python/File Handling/file.py
+++
@@ -1,4 +0,0 @@
-with open("data.txt", 'r') as file:
-    text = file.readlines()
-    for line in text:
-        print(line)
--- Python/turtle.py
+++
@@ -1,7 +0,0 @@
-import turtle
-
-turtle.circle(100)
-turtle.color("blue")
-turtle.left(200)
-turtle.pensize(10)
-turtle.write("Hello World!")
