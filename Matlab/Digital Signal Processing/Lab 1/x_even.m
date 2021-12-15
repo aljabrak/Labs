22c57417501bef674ce557d2e35c4bbe6cc4f518
Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-12-15 16:28:46 +0500

    Delete Matlab directory

--- Matlab/Digital Signal Processing/Lab 1/x_even.m
+++
@@ -1,5 +0,0 @@
-function xe = x_even(x)
-x_reverse = fliplr(x);% time reversal
-xe = 0.5*(x + x_reverse); %even component
-
-end
--- Matlab/Digital Signal Processing/Lab 1/x_odd.m
+++
@@ -1,5 +0,0 @@
-function xo = x_odd(x)
-x_reverse = fliplr(x);% time reversal
-xo = 0.5*(x - x_reverse); %odd component
-
-end
