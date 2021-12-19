03a6535482a1bf91f05a5a6c0d70c3fe57d9b44b
Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-12-19 18:49:19 +0500

    Initial Commit

--- Digital Signal Processing. (MATLAB)/Project/main.m
+++ Digital Signal Processing. (MATLAB)/Project/main.m
@@ -1,10 +1,15 @@
-clear
+clear all;
 clc
 
-[y, Fs, nbits] = wavread('feynman.wav');
+[y, Fs] = audioread('feynman.wav');
 sound(y, Fs);
+% pause(30);
 
-Mean = 0.00;
-Variance = 0.005;
-z = imnoise(y,'Gaussian', Mean, Variance);
-sound(z, Fs);
+% Mean = 0.00;
+% Variance = 0.0005;
+% z = imnoise(y,'Gaussian', Mean, Variance);
+% sound(z, Fs);
+pause(30);
+
+x = quantization(y, 8);
+sound(x, Fs);
