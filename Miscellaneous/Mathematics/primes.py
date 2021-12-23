514d2756b54c77e3deb16f16c6bfb05452be2725
Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2021-12-23 12:56:44 +0500

    append p

--- Miscellaneous/Mathematics/primes.py
+++ Miscellaneous/Mathematics/primes.py
@@ -2,25 +2,29 @@ from numpy import *
 from math import *
 from matplotlib.pyplot import *
 
+
 def Prime(N):
     P = []
-    for p in range(2, N + 1):
+    for p in range(0, N + 1):
         if p > 1:
-            for a in range(2, N + 1):
-                if p % a == 0 and p != 2:
-                    # print(p)
+            for a in range(2, p):
+                if (p % a) == 0:
                     break
-                else:
-                    # print("P = ", p)
-                    P.append(p)
-    
+            else:
+                P.append(p)
     P = list(dict.fromkeys(P))
     return P
+
 
 def main():
     N = int(input("Enter Number: N = "))
     primes = Prime(N)
     print(primes)
+    natural = [i for i in range(1, N + 1)]
+    figure, axis = subplots(2, 1)
+    axis[0].stem(primes)
+    axis[1].stem(natural)
+    show()
 
 
 main()
