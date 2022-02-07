e763d17deb28b175ac2eaeb5205d55d6e527a775
Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2022-02-07 13:27:56 +0500

    Create newton_raphson_method.py

---
+++ Numerical Methods/newton_raphson_method.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def newton_method(f, df, x0, n):
+    """
+    f(x) = Function,
+    df(x) = Derivative of function,
+    x0 = Inspected root,
+    n = Number of iterations.
+    """
+    x = x0
+    for a in range(1, n):
+        x -= f(x) / df(x)
+    
+    return x
+
+
+f = lambda x: x**2 + 4*x + 1
+df = lambda x: 2*x + 4
+x0 = 0
+n = 10000
+root = newton_method(f, df, x0, n)
+print(root)
+
+err = (1 - (root / (-2 + np.sqrt(3)))) * 100
+print("Error = {a:.3e}%".format(a = err))
