df01599c8ec3315274e4804b96f210bd5dabd4e9
Saud Zahir <76210541+saudzahirr@users.noreply.github.com>
2022-02-08 12:19:02 +0500

    Update newton_raphson_method.py

--- Numerical Methods/newton_raphson_method.py
+++ Numerical Methods/newton_raphson_method.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-def newton_method(f, df, x0, n):
+def newtons_method(f, df, x0, n):
     """
     f(x) = Function,
     df(x) = Derivative of function,
-    x0 = Inspected root,
+    x0 = Inspected (guessed) root,
     n = Number of iterations.
     """
     x = x0
@@ -19,7 +19,7 @@ f = lambda x: x**2 + 4*x + 1
 df = lambda x: 2*x + 4
 x0 = 0
 n = 10000
-root = newton_method(f, df, x0, n)
+root = newtons_method(f, df, x0, n)
 print(root)
 
 err = (1 - (root / (-2 + np.sqrt(3)))) * 100
